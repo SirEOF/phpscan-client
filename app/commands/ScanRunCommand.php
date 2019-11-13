4cfe7decbfdb8509f8cc9a31dbd2a5c95ee7e5f9
Amin eeXBee <me@eexbee.com>
2019-11-13 18:23:38 +0330

    1.checks manifest with server, 2.zip php files keeping directory structure, 3.uploads zip files to S3

--- app/commands/ScanRunCommand.php
+++ app/commands/ScanRunCommand.php
@@ -9,6 +9,7 @@ use Symfony\Component\Console\Formatter\OutputFormatterStyle;
 use Symfony\Component\Finder\Finder;
 use PhpParser\ParserFactory;
 use PhpParser\Node\Stmt;
+use Aws\S3\S3Client;
 
 class ScanRunCommand extends Command
 {
@@ -18,9 +19,19 @@ class ScanRunCommand extends Command
     /** @var \Symfony\Component\Console\Style\SymfonyStyle */
     private $io;
 
+    private $mainAppBaseURI;
+
     public function __construct($name = null)
     {
         parent::__construct($name);
+        Dotenv\Dotenv::create(__DIR__ . "/../../")->load();
+
+        if ($_ENV['ENV'] === 'local') {
+            $this->mainAppBaseURI = $_ENV['MAIN_APP_BASE_URI_LOCAL'];
+        } else if ($_ENV['ENV'] === 'prod') {
+            $this->mainAppBaseURI = $_ENV['MAIN_APP_BASE_URI_PROD'];
+        }
+
     }
 
     protected function configure()
@@ -51,10 +62,69 @@ class ScanRunCommand extends Command
 
         $phpFiles = $this->gatherPhpFiles($baseDirectory);
         $manifest = $this->hashes($phpFiles);
-        $io->table(['hash', 'size'], $manifest);
-//        foreach ($phpFiles as $file) {
-//            $io->writeln($file);
-//        }
+        $io->writeln('Checking manifest with scan server');
+//        file_put_contents('manifest.json', json_encode($manifest));
+//        $result = $this->checkManifestWithServer($manifest, $apiKey);
+        $result['covered'] = false;
+        if (!isset($result['covered'])) {
+            $io->error('API call to front server failed');
+            return;
+        }
+
+        if ($result['covered']) {
+            // call $result['status'] url to get report page url
+        } else {
+//            $s3Token = $result['token'];
+//            $s3Bucket = $result['bucket'];
+//            $s3Region = $result['region'];
+//            $s3Token = 'WhKnN7KApQF6rUNveL7diQQAK6Bt1jkZfV2XYU6z';
+            $s3Bucket = 'kaarbon-test';
+            $s3Region = 'eu-central-1';
+
+            $io->writeln('Compressing...');
+            $zipFileNamePath = "archive/" . $apiKey . '-' . time() . ".zip";
+            $zipFileName = $apiKey . '-' . time() . ".zip";
+            if (!$this->compress($phpFiles, $baseDirectory, $zipFileNamePath)) {
+                $io->error('Compression failed due to available disk space or permission');
+                return;
+            }
+
+            $io->writeln('Uploading...');
+            $uploadResult = $this->uploadToS3($zipFileName, $s3Bucket, $s3Region, $zipFileName);
+            if ($uploadResult['status'] !== 'done') {
+                $io->error('Upload failed');
+                return;
+            }
+
+            $io->writeln('Acknowledging server...');
+            // get s3_token, s3_bucket, s3_region from $result and
+            // zip php files and
+            // upload to s3 and
+            // acknowledge server
+        }
+
+    }
+
+    private function checkManifestWithServer($manifest, $apiKey) {
+        $manifest['manifest_key'] = sha1(str_shuffle('abcdefghigklmnopqrstwxyz1234567890'));
+
+        $client = new \GuzzleHttp\Client([
+            'base_uri' => $this->mainAppBaseURI,
+            'verify' => false,
+            'headers' => [
+                'Content-Type'  => 'application/x-www-form-urlencoded',
+                'Accept'        => 'application/json',
+                'Authorization' => 'Bearer '.$apiKey
+            ]
+        ]);
+
+        $uri = "/api/job/check-manifest";
+        $response = $client->post($uri, [
+            GuzzleHttp\RequestOptions::JSON => $manifest
+        ], [
+            'timeout' => 600
+        ]);
+        return json_decode($response->getBody(), true);
 
     }
 
@@ -85,7 +155,9 @@ class ScanRunCommand extends Command
         $result = [];
         foreach ($files as $filePath) {
             $size = filesize($filePath);
-            if($this->is_php($filePath, $parser)) {
+//            if($this->is_php($filePath, $parser)) {
+            if(1) {
+                $this->io->writeln($filePath);
                 $result[] = [$filePath,  $size];
             }
 
@@ -106,6 +178,7 @@ class ScanRunCommand extends Command
         }
         try {
             $ast = $parser->parse($content);
+            $content = null;
             $is_php=false;
             foreach ($ast as $node)
             {
@@ -125,6 +198,7 @@ class ScanRunCommand extends Command
             return false;
         }
 //        if(self::DEBUG)  dump("at the end so is php is true");
+        $content = null;
         return true;
     }
 
@@ -138,19 +212,96 @@ class ScanRunCommand extends Command
     }
 
     private function hashes($files) {
-        $result = [];
         $hashes = [];
+        $sizes = [];
+        $paths = [];
         foreach($files as $key => $value) {
+            error_log(json_encode($value));
             if(is_array($value)) {
                 $hash = $this->hash($value[0]);
                 if(in_array($hash, $hashes))
                     continue;
                 $hashes[] = $hash;
-                $result[$value[0]][0] = $hash;
-                $result[$value[0]][1] = $value[1];
+                $sizes[] = (int)$value[1]/(1024*1024);
+                $paths[] = $value[0];
+//                $result[$value[0]][0] = $hash;
+//                $result[$value[0]][1] = $value[1];
             }
         }
-        return $result;
+        return [
+            'hashes'    => $hashes,
+            'sizes'     => $sizes,
+            'paths'     => $paths
+        ];
+    }
+
+    /**
+     * @param string $path
+     * @param string $bucket
+     * @param string $region
+     * @param string[80] $key
+     * @return array
+     */
+    private function uploadToS3($path, $bucket, $region, $key) {
+        $s3Client = new S3Client([
+            'region' => $region,
+            'version' => 'latest'
+        ]);
+        $response = [];
+        $bucketExistence = $s3Client->doesBucketExist($bucket);
+        if ($bucketExistence && !$s3Client->doesObjectExist($bucket, $key)) {
+            try {
+                $params = [
+                    'Key'       => $key,
+                    'Bucket'    => $bucket,
+                    'Body'      => file_get_contents($path)
+                ];
+                $result = $s3Client->putObject($params);
+                $response['status'] = 'done';
+            }
+            catch (\Aws\Exception\AwsException $e) {
+                $response['status'] = 'error';
+                $response['message'] = 'could not upload object to bucket! '.$bucket
+                    ." ".$key;
+            }
+        } else {
+            if(!$bucketExistence) {
+                $response['status'] = 'error';
+                $response['message'] = 'bucket does not exist! '.$bucket
+                    ." ".$key;
+            }
+            else {
+                $response['status'] = 'ok';
+                $response['message'] = 'key already existed in the bucket! '.$bucket
+                    ." ".$key;
+            }
+        }
+        return $response;
+    }
+
+
+    /**
+     * @param $files
+     * @param $directory
+     * @param $fileName
+     * @return bool
+     */
+    private function compress($files, $directory, $fileName) {
+        $zippy = \Alchemy\Zippy\Zippy::load();
+        $params = [];
+        $relativePaths = [];
+        foreach($files as $file) {
+            $relativePaths[] = str_replace($directory."/", "", $file[0]);
+            $params[] = $file[0];
+        }
+
+        try {
+            $archive = $zippy->create($fileName, $params);
+            return true;
+        } catch (\Alchemy\Zippy\Exception\RuntimeException $e) {
+            return false;
+        }
+
     }
 
 }
